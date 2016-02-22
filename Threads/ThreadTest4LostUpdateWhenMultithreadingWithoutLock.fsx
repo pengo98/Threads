1590c942604aad279399aff44c0f5d6ae2c218e3
nelson.lin <nelson.lin@jet.com>
2016-02-21 22:59:02 -0500

    Use ThreadPool.QueueUserWorkItem to start background tasks and demonstrate lost update when multithreading without locking.

---
+++ Threads/ThreadTest4LostUpdateWhenMultithreadingWithoutLock.fsx
@@ -0,0 +1,58 @@
+﻿(*
+ Lost update is possible if multiple threads update a shared variable without a lock. 
+*)
+
+(*
+Part1: Multithreading without locking (result is indeterministic) 
+*)
+open System.Threading
+let mutable x = 0
+let increment() = x <- x + 1
+let decrement() = x <- x - 1
+
+
+let increment1000 = 
+    fun () ->  
+    [1..10000] |> List.iter(fun _ -> increment())
+    printfn "Increment Thread %i completed" Thread.CurrentThread.ManagedThreadId
+
+let decrement1000 =
+    fun () ->
+    [1..10000] |> List.iter(fun _ -> decrement())
+    printfn "Decrement Thread %i completed" Thread.CurrentThread.ManagedThreadId
+
+printfn "Inital x = %i" x
+let t = new Thread(increment1000)
+t.Start()
+
+decrement1000() // runs on main thread
+
+t.Join()
+printfn "Final x = %i" x
+
+
+
+(*
+Part2: Proper locking  
+*)
+//open System.Threading
+//let mutable y = 0
+//let sync = ref 0
+//let increment () = lock(sync) (fun _ -> y <- y + 1)
+//let decrement () = lock(sync) (fun _ -> y <- y - 1)
+//
+//printfn "x = %i" y
+//
+//ThreadPool.QueueUserWorkItem(fun _ -> 
+//                                [1..1000] 
+//                                |> List.iter(fun _ -> increment() )
+//                                printfn "Thread %i done" Thread.CurrentThread.ManagedThreadId   
+//                             ) |> ignore
+//
+//ThreadPool.QueueUserWorkItem(fun _ -> 
+//                                [1..1000] 
+//                                |> List.iter(fun _ -> decrement() )
+//                                printfn "Thread %i done" Thread.CurrentThread.ManagedThreadId   
+//                             ) |> ignore
+//
+//printfn "x = %i" y
---
+++ Threads/ThreadTest4QueueWorkItem.fsx
@@ -0,0 +1,18 @@
+﻿open System
+open System.Threading
+
+let synRoot = ref 0
+let presenterFunction() =
+    let id = Thread.CurrentThread.ManagedThreadId
+    printfn "I (%d) starting, please do not interrupt!" id
+    Thread.Sleep 1000
+    printfn "I (%d) finished." id
+
+
+let longTalk() =
+    lock(synRoot) (fun() -> presenterFunction())
+
+ThreadPool.QueueUserWorkItem(fun _ -> longTalk())
+ThreadPool.QueueUserWorkItem(fun _ -> longTalk())
+ThreadPool.QueueUserWorkItem(fun _ -> longTalk())
+
