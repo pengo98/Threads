5a7b62f4cd014d503d23c59d34699132d824c30d
nelson.lin <nelson.lin@jet.com>
2016-02-22 23:58:14 -0500

    Basic WaitHandle

---
+++ Threads/ThreadTest9BasicWaitHandle.fsx
@@ -0,0 +1,13 @@
+﻿open System.Threading
+
+let waitHandle = new AutoResetEvent(false)
+
+let waiter() =
+    printfn "Waiting..."
+    waitHandle.WaitOne() |> ignore
+    printfn "Notified"
+
+(new Thread(waiter)).Start()
+Thread.Sleep 1000
+waitHandle.Set()
+
