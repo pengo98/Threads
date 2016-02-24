939162746edb9cb159ad592f5f8442a349521825
nelson.lin <nelson.lin@jet.com>
2016-02-23 20:29:34 -0500

    Add multiple waiters.

--- Threads/ThreadTest9BasicWaitHandle.fsx
+++ Threads/ThreadTest9BasicWaitHandle.fsx
@@ -3,11 +3,26 @@
 let waitHandle = new AutoResetEvent(false)
 
 let waiter() =
-    printfn "Waiting..."
+    printfn "Waiting1..."
     waitHandle.WaitOne() |> ignore
-    printfn "Notified"
+    printfn "Notified2"
+    waitHandle.WaitOne() |> ignore
+    printfn "Notified3"
+    waitHandle.WaitOne() |> ignore
+    printfn "Notified4"
+
+let waiter2() =
+    printfn "Waiting5..."
+    waitHandle.WaitOne() |> ignore
+    printfn "Notified6"
+    waitHandle.WaitOne() |> ignore
+    printfn "Notified7"
+    waitHandle.WaitOne() |> ignore
+    printfn "Notified8"
 
 (new Thread(waiter)).Start()
+(new Thread(waiter2)).Start()
+
 Thread.Sleep 1000
 waitHandle.Set()
 
